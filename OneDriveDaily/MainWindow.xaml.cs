0944f0386586c980988f5d50726b8a942165a43f
James Clarkson <jclarkson@mediaresources.com>
2022-09-11 15:49:19 -0400

    First

---
+++ OneDriveDaily/MainWindow.xaml.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Drawing;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace OneDriveDaily
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public class TestyTest
+    {
+        public TestyTest(TestyTest2 uri)
+        {
+            ImageUri = uri.Name;
+            Size = uri.Size + " KB";
+            Image = File.ReadAllBytes(uri.Name);
+
+            var temp = BitmapFrame.Create(new MemoryStream(Image), BitmapCreateOptions.DelayCreation, BitmapCacheOption.None);
+            Resolution = $"{temp.PixelWidth} x {temp.PixelHeight}";
+            temp = null;
+        }
+
+        public string ImageUri { get; set; }
+
+        public string Resolution { get; set; }
+
+        public string Size { get; set; }
+
+        public byte[] Image { get; set; }
+    }
+
+    public class TestyTest2
+    {
+        public string Name { get; set; }
+        public string Resolution { get; set; }
+        public long Size { get; set; }
+    }
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            index = 0;
+
+            ChooseFiles();
+        }
+
+        //public List<string> m_arrFiles;
+        private string[] Paths = new string[5] { ".bmp", ".jpg", ".jpeg", ".png", ".jfif" };
+
+
+        public static DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(TestyTest), typeof(MainWindow));
+        public TestyTest item
+        {
+            get { return (TestyTest)GetValue(TextProperty); }
+            set { this.SetValue(TextProperty, value); }
+        }
+
+        public static DependencyProperty TextProperty2 = DependencyProperty.Register("Text7", typeof(string), typeof(MainWindow));
+        public string item2
+        {
+            get { return (string)GetValue(TextProperty2); }
+            set { this.SetValue(TextProperty2, value); }
+        }
+
+        public static DependencyProperty ListProperty = DependencyProperty.Register("Text4", typeof(ObservableCollection<TestyTest>), typeof(MainWindow));
+        public ObservableCollection<TestyTest> m_arrFiles
+        {
+            get { return (ObservableCollection<TestyTest>)GetValue(ListProperty); }
+            set { this.SetValue(ListProperty, value); }
+        }
+
+        public static DependencyProperty ListProperty2 = DependencyProperty.Register("Text5", typeof(int), typeof(MainWindow));
+        public int index
+        {
+            get { return (int)GetValue(ListProperty2); }
+            set { this.SetValue(ListProperty2, value); }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler == null) return;
+            handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Window1 window = null;
+
+        private void ChooseFiles()
+        {
+            var arrFiles = new List<TestyTest2>();
+
+            var systemPath = System.Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData
+            );
+            var complete = System.IO.Path.Combine(systemPath, "files2.txt");
+            var strFolders = "";
+            try
+            {
+                using (StreamReader iso = new StreamReader(complete))
+                {
+                    strFolders = iso.ReadLine();
+                }
+                if (strFolders.Length == 0) return;
+            }
+            catch
+            {
+                return;
+            }
+
+            var arrFolders = strFolders.Split(',');
+
+            foreach (var item in arrFolders)
+            {
+                arrFiles.AddRange(CountFiles(new DirectoryInfo(item.Trim()).GetFileSystemInfos()));
+            }
+
+            //FileSystemInfo[] infos = new DirectoryInfo("C:\\Users\\James\\OneDrive\\SkyDrive camera roll").GetFileSystemInfos();
+            //arrFiles.AddRange(CountFiles(infos));
+
+            //FileSystemInfo[] infos2 = new DirectoryInfo("C:\\Users\\James\\OneDrive\\Pictures\\Pictures").GetFileSystemInfos();
+            //arrFiles.AddRange(CountFiles(infos2));
+
+            m_arrFiles = new ObservableCollection<TestyTest>();
+            arrFiles = arrFiles.OrderBy(c => System.IO.Path.GetFileNameWithoutExtension(c.Name)).ToList();
+
+            foreach (var item in arrFiles)
+            {
+                m_arrFiles.Add(new TestyTest(item));
+            }
+
+            //m_arrFiles = new ObservableCollection<string>(arrFiles.OrderBy(System.IO.Path.GetFileNameWithoutExtension).ToList());
+
+            //Frank.ItemsSource = m_arrFiles;
+        }
+        private List<TestyTest2> CountFiles(FileSystemInfo[] infos)
+        {
+            List<TestyTest2> files = new List<TestyTest2>();
+            for (int i = 0; i < infos.Length; i++)
+            {
+                if (infos[i] is DirectoryInfo)
+                {
+                    files.AddRange(CountFiles(((DirectoryInfo)infos[i]).GetFileSystemInfos()));
+                }
+                else if (infos[i] is FileInfo)
+                {
+                    if (Paths.Contains((((FileInfo)infos[i]).Extension).ToLower()))
+                    {
+                        var date = infos[i].LastWriteTime;
+                        var today = DateTime.Today;
+
+                        if (today.Day == date.Day && today.Month == date.Month)
+                            files.Add(new TestyTest2() { Name = infos[i].FullName, Size = (infos[i] as FileInfo).Length / 1024 });
+                    }
+                }
+            }
+            return files;
+        }
+
+        private void Frank_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            index = this.Test.SelectedIndex == -1 ? 0 : this.Test.SelectedIndex;
+            LastIndex = index;
+            //this.Frank.SelectedIndex = index;
+            //this.Frank.SelectedItem = Frank.Items[index];
+            //item = (TestyTest)Frank.Items[index];
+            item2 = this.m_arrFiles[index].ImageUri;
+
+            if (window == null || window.isClosed)
+            {
+                //window = new Window1(item.ImageUri);
+                window = new Window1(item2);
+                window.Show();
+            }
+            else
+            {
+                //window.Update(item.ImageUri);
+                window.Update(item2);
+                window.Show();
+            }
+        }
+        int LastIndex = 0;
+        private void Image_KeyDown(object sender, KeyEventArgs e)
+        {
+            var name = "";
+
+
+            if (e.Key == Key.Enter)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo()
+                {
+                    UseShellExecute = true,
+                    FileName = item.ImageUri
+                };
+                Process.Start(startInfo);
+            }
+            else if (e.Key == Key.Delete)
+            {
+                var temp = item.ImageUri;
+
+                //window.Close();
+                //window = null;
+                //index = this.Frank.SelectedIndex;
+                var localIndex = index;
+                m_arrFiles.Remove(item);
+                OnPropertyChanged(nameof(m_arrFiles));
+
+                //Task.Run(() =>
+                //{
+                    //Thread.Sleep(1000);
+
+                    //File.Delete(temp);
+                //});
+
+                index = localIndex;
+                //this.Test.SelectedIndex = localIndex;
+                //this.Test.SelectedItem = Frank.Items[localIndex];
+                item2 = this.m_arrFiles[index].ImageUri;
+                item = (TestyTest)Test.Items[localIndex];
+            }
+            else if (e.Key == Key.Left)
+            {
+                if (index == 0) { }
+                else
+                {
+                    if (Math.Abs(index - LastIndex) > 1) index = LastIndex;
+        
+
+                    index--;
+                    item = m_arrFiles[index];
+                    name = item.ImageUri;
+                    //Test.SelectedValue = item.ImageUri;
+                    //this.Frank.SelectedIndex = index;
+                    this.Test.SelectedItem = item;
+                    item2 = this.m_arrFiles[index].ImageUri;
+                    Test.ScrollIntoView(item);
+                }
+            }
+            else if (e.Key == Key.Right)
+            {
+                if (index == this.Test.Items.Count - 1) { }
+                else
+                {
+                    if (Math.Abs(index - LastIndex) > 1) index = LastIndex;
+
+
+                    index++;
+                    item = m_arrFiles[index];
+                    name = item.ImageUri;
+                    //this.Frank.SelectedIndex = index;
+                    //Frank.SelectedValue = item.ImageUri;
+                    this.Test.SelectedItem = item;
+                    item2 = this.m_arrFiles[index].ImageUri;
+                    Test.ScrollIntoView(item);
+                }
+            }
+            else if (e.Key == Key.Up)
+            {
+                if (index <= 3) { }
+                else
+                {
+                    if (Math.Abs(index - LastIndex) > 4) index = LastIndex;
+
+
+                    index -= 4;
+                    item = m_arrFiles[index];
+                    name = item.ImageUri;
+                    //this.Test.SelectedIndex = index;
+                    //Test.SelectedValue = item.ImageUri;
+                    item2 = this.m_arrFiles[index].ImageUri;
+                    this.Test.SelectedItem = item;
+                    Test.ScrollIntoView(item);
+                }
+            }
+            else if (e.Key == Key.Down)
+            {
+                if (index >= this.Test.Items.Count - 5) { }
+                else
+                {
+                    if (Math.Abs(index - LastIndex) > 4) index = LastIndex;
+
+                    index += 4;
+                    item = m_arrFiles[index];
+                    name = item.ImageUri;
+                    //this.Test.SelectedIndex = index;
+                    //Test.SelectedValue = item.ImageUri;
+                    this.Test.SelectedItem = item;
+                    item2 = this.m_arrFiles[index].ImageUri;
+                    Test.ScrollIntoView(item);
+                }
+            }
+
+            if (name.Length > 0)
+            {
+                if (window == null || window.isClosed)
+                {
+                    window = new Window1(name);
+                    window.Show();
+                }
+                else
+                {
+                    window.Update(name);
+                    window.Show();
+                }
+            }
+
+            LastIndex = index;
+            e.Handled = true;
+        }
+
+        private void Frank_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            /*index = this.Frank.SelectedIndex;
+            if (window == null)
+            {
+                if (e.AddedItems.Count > 0)
+                {
+                    window = new Window1(((TestyTest)(e.AddedItems[0])).ImageUri);
+                    window.Show();
+                }
+            }
+            else
+            {
+                if (window.isClosed)
+                {
+                    window = new Window1(((TestyTest)(e.AddedItems[0])).ImageUri);
+                    window.Show();
+                }
+                else
+                {
+                    if (e.AddedItems.Count > 0)
+                    {
+                        window.Update(((TestyTest)(e.AddedItems[0])).ImageUri);
+                        window.Show();
+                    }
+                }
+            }
+
+            window?.UpdateLayout();*/
+        }
+    }
+}
---
+++ OneDriveDaily/Window1.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OneDriveDaily
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window, INotifyPropertyChanged
+    {
+
+        public static DependencyProperty TextProperty = DependencyProperty.Register("Text2", typeof(byte[]), typeof(Window1));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool isClosed = true;
+        public byte[] BetterStuff
+        {
+            get { return (byte[])GetValue(TextProperty); }
+            set { this.SetValue(TextProperty, value); }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler == null) return;
+            handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+        public Window1(string stuff)
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            BetterStuff = File.ReadAllBytes(stuff);
+
+            isClosed = false;
+
+        }
+
+        public void Update(string stuff)
+        {
+            BetterStuff = File.ReadAllBytes(stuff);
+            OnPropertyChanged(nameof(BetterStuff)); 
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            isClosed = true;
+        }
+    }
+}
